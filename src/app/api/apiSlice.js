96976041f2a6bff5ad6b82617c68a4151904e028
Timothy Adams <adams.timothy.d@gmail.com>
2025-10-29 12:42:15 -0400

    fix: update api resource to support deployments

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setCredentials, logout } from '../../features/auth/authSlice';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:3500/api',
+  baseUrl: `${process.env.API_URL}/api`,
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
